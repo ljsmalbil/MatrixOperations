0ed423dec57e8a43cd67d0d5bb8788c9f83f6265
ljsmalbil <louksmalbil@gmail.com>
2021-03-13 21:33:56 +0100

    to be improved

--- main.cpp
+++ main.cpp
@@ -1,23 +1,71 @@
 #include <iostream>
+#include <string>
 
 
 // This function creates one grid block
 void grid_block() {
-     std::cout << "+-----------+\n";
-     std::cout << "+           +\n";
-     std::cout << "+           +\n";
-     std::cout << "+           +\n";
-     std::cout << "+-----------+\n";
 
+     std::cout << ":          :\n";
+     std::cout << ":          :\n";
+     std::cout << ":          :\n";
+     std::cout << ":..........:\n";
 }
 
 int main() {
 
-    for (int i = 0; i < 5; i++) {
-        grid_block();
+    for (int i = 0; i < 1; i++) {
+
+        for (int j = 0; j < 100; j++) {
+            std::cout << ".";
+
+        }
         std::cout << "\n";
 
+        for (int j = 0; j < 100; j++) {
+            if (j % 10 == 0) {
+                std::cout << ":         :\n";
+                std::cout << ":         :\n";
+                std::cout << ":         :\n";
+                std::cout << ":         :\n";
+
+            }
+        }
+
+
+
+        for (int j = 0; j < 5; j++) {
+            std::cout << ".";
+
+            }
+
     }
+
+
+    std::cout << "\n";
+//
+//    for (int i = 0; i < 3; i++) {
+//        for (int j = 0; j < 3; j++) {
+//            std::cout << ":         :\n";
+//        }
+//    }
+//
+//
+//    for (int i = 0; i < 11; i++) {
+//        std::cout << ".";
+//    }
+
+
+
+//
+//        for (int j = 0; j < 4; j++) {
+//            std::cout << ":            :\n";
+//        }
+
+        // std::cout << "..............";
+        // std::cout << i;
+
+
+
     return 0;
 
 }
