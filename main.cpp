a299845bcec6d673aad02bbacedba60f41b3ba40
ljsmalbil <louksmalbil@gmail.com>
2021-03-14 12:20:52 +0100

    improved grid structure added

--- main.cpp
+++ main.cpp
@@ -3,68 +3,46 @@
 
 
 // This function creates one grid block
-void grid_block() {
+void grid_block(int rows, int cols) {
+    cols = cols * 25;
 
-     std::cout << ":          :\n";
-     std::cout << ":          :\n";
-     std::cout << ":          :\n";
-     std::cout << ":..........:\n";
+    for (int i = 0; i < rows; i++) {
+
+        // output horizontal lines
+        for (int j = 0; j < cols; j++) {
+            if (j % 20 == 0) {
+                std::cout << ".";
+            }
+            else {
+                std::cout << " ";
+            }
+        }
+
+        // output vertical lines
+        for (int j = 0; j < 5; j++) {
+            std::cout << "\n";
+        }
+
+        // output last row
+        if (i == rows - 1) {
+            for (int j = 0; j < cols; j++) {
+                if (j % 20 == 0) {
+                    std::cout << ".";
+                }
+                else {
+                    std::cout << " ";
+                }
+            }
+        }
+
+
+        std::cout << "\n";
+    }
 }
 
 int main() {
 
-    for (int i = 0; i < 1; i++) {
-
-        for (int j = 0; j < 100; j++) {
-            std::cout << ".";
-
-        }
-        std::cout << "\n";
-
-        for (int j = 0; j < 100; j++) {
-            if (j % 10 == 0) {
-                std::cout << ":         :\n";
-                std::cout << ":         :\n";
-                std::cout << ":         :\n";
-                std::cout << ":         :\n";
-
-            }
-        }
-
-
-
-        for (int j = 0; j < 5; j++) {
-            std::cout << ".";
-
-            }
-
-    }
-
-
-    std::cout << "\n";
-//
-//    for (int i = 0; i < 3; i++) {
-//        for (int j = 0; j < 3; j++) {
-//            std::cout << ":         :\n";
-//        }
-//    }
-//
-//
-//    for (int i = 0; i < 11; i++) {
-//        std::cout << ".";
-//    }
-
-
-
-//
-//        for (int j = 0; j < 4; j++) {
-//            std::cout << ":            :\n";
-//        }
-
-        // std::cout << "..............";
-        // std::cout << i;
-
-
+    grid_block(4, 4);
 
     return 0;
 
